e5355433ba39ec9aa51410bbe7d1a3372d49ad87
pmarkgraf-bionaut <89817253+pmarkgraf-bionaut@users.noreply.github.com>
2021-11-04 10:39:45 -0700

    r Extract functions from match branches

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,18 @@
 pub enum Category {
-    Chance,
     Yatzy,
+    Chance,
 }
 pub fn yatzy(category: Category, roll: Vec<u32>) -> u32 {
     match category {
-        Category::Chance => roll.into_iter().fold(0, |sum, x| sum + x),
-        Category::Yatzy => 50,
+        Category::Yatzy => score_yatzy(),
+        Category::Chance => score_chance(roll),
     }
+}
+
+fn score_yatzy() -> u32 {
+    50
+}
+
+fn score_chance(roll: Vec<u32>) -> u32 {
+    roll.into_iter().fold(0, |sum, x| sum + x)
 }
