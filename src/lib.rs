f6e4d3ef0acc5af404af01f82c3d9da091f597b3
pmarkgraf-bionaut <89817253+pmarkgraf-bionaut@users.noreply.github.com>
2021-11-16 11:20:21 -0800

    r Fixed len_zero warning, per Clippy

--- src/lib.rs
+++ src/lib.rs
@@ -71,12 +71,12 @@ fn sum_rolls(roll: Vec<u32>) -> u32 {
 fn two_of_some_die(roll: &[u32]) -> bool {
     let mut twice_rolled_histogram = create_roll_histogram(&roll);
     twice_rolled_histogram.retain(|_, v| *v == 2);
-    twice_rolled_histogram.len() > 0
+    ! twice_rolled_histogram.is_empty()
  }
  fn three_of_some_die(roll: &[u32]) -> bool {
     let mut thrice_rolled_histogram = create_roll_histogram(&roll);
     thrice_rolled_histogram.retain(|_, v| *v == 3);
-    thrice_rolled_histogram.len() > 0
+    ! thrice_rolled_histogram.is_empty()
  }
  
 fn create_roll_histogram(roll: &[u32]) -> HashMap<u32, u32> {
